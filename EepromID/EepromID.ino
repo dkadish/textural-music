de6e8aa3aaf6c90e527b297ab2c1162cdbd34b5c
David Kadish <david.kadish@gmail.com>
2015-03-15 19:05:06 -0700

    Added code to write the eeprom ID on an arduino

---
+++ EepromID/EepromID.ino
@@ -0,0 +1,13 @@
+#include <EEPROM.h>
+
+char UNIQUE_ID = 2;
+
+void setup() {
+  // put your setup code here, to run once:
+  EEPROM.write(0, UNIQUE_ID);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly: 
+  
+}
